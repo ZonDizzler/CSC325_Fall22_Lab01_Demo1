c185fc9fe78345c87fa9fcb3c149234a30f920d2
ZonDizzler <94850886+ZonDizzler@users.noreply.github.com>
2022-09-01 10:47:35 -0400

    Add files via upload

---
+++ CSC325_Fall22_Lab01_Demo1/src/main/java/com/mycompany/csc325_fall22_lab01_demo1/NewClass.java
@@ -0,0 +1,20 @@
+package com.mycompany.csc325_fall22_lab01_demo1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author John Distler (distjc@farmingdale.edu)
+ */
+public class NewClass {
+    
+        public static void main(String[] args) {
+            
+            System.out.println("Minions 2: Rise of Gru");
+        }
+
+}
